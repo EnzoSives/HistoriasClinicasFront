6cf662551fe2908cada7109eb47fc67da5a0e2ad
EnzoSives <73887784+EnzoSives@users.noreply.github.com>
2024-09-04 15:02:25 -0300

    Update axios.ts

--- src/axios.ts
+++ src/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 
-axios.defaults.baseURL = 'http://149.50.137.224:3001';
+axios.defaults.baseURL = 'http://149.50.137.224:3000';
 // axios.defaults.baseURL = 'https://secretaria-privada.onrender.com';
